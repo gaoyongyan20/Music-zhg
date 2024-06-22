dd48bc1420ee97d8fbe398ea180bb6319074995d
2022051615276gaoyongyan <2327014830@qq.com>
2024-06-22 21:15:22 +0800

    test3:QString Lyrics::changeTimeShow(QString  timestamp)

--- lyrics.cpp
+++ lyrics.cpp
@@ -29,7 +29,63 @@ int Lyrics::getIndexByKey(QString key)
 
 void Lyrics::spiltLyrics() {}
 
+//00:00    00:00.00
 QString Lyrics::changeTimeShow(QString  timestamp) {
-    return timestamp;
+    QString leftString, midString, rightString;
+    int leftInt, midInt, rightInt;
+    bool o, k, n;
+    int totalMillisecond;
+    QString totalTimestamp;
+
+    //find the last one to appear index of'.'
+    int index = timestamp.QString::lastIndexOf('.');
+    //no '.'appear in the timestamp
+    if (index == -1) {
+        //"11"of"11:22"
+        leftString = timestamp.left(2);
+        //"22"of"11:22"
+        rightString = timestamp.right(2);
+        //If ok is not nullptr,
+        //failure is reported by setting *ok to false,
+        //and success by setting *ok to true.
+        leftInt = leftString.toInt(&o, 10);
+        if (o) {
+            rightInt = rightString.toInt(&k, 10);
+            if (k) {
+                totalMillisecond = (leftInt * 60 + rightInt) * 1000;
+                totalTimestamp = QString::number(totalMillisecond);
+                return totalTimestamp;
+            } else {
+                return "k false";
+            }
+        } else {
+            return "o false";
+        }
+    } else {
+        //"."apperear in the timestamp
+        //"11"of"11:22.33"
+        leftString = timestamp.left(2);
+        //"22"of"11:22.33"
+        midString = timestamp.mid(4, 2);
+        //"33"of"11:22.33"
+        rightString = timestamp.right(2);
+
+        leftInt = leftString.toInt(&o, 10);
+        if (o) {
+            rightInt = rightString.toInt(&k, 10);
+            if (k) {
+                midInt = midString.toInt(&n, 10);
+                if (n) {
+                    totalMillisecond = (leftInt * 60 + midInt) * 1000 + rightInt;
+                    totalTimestamp = QString::number(totalMillisecond);
+                    return totalTimestamp;
+                }
+            } else {
+                return "k false";
+            }
+        } else {
+            return "o false";
+        }
+    }
 }
 
